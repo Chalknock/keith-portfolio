f6516b745255584ecc1f441f74a1d9922fd02c7e
KEITH ANGELO TUMANENG <katumaneng24@gmail.com>
2026-04-07 21:09:32 +0800

    add experience and reconfigure config files

--- next.config.ts
+++ next.config.ts
@@ -3,13 +3,13 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-  output: "export",
-  images: {
-    unoptimized: true,
-  },
+  // output: "export",
+  // images: {
+  //   unoptimized: true,
+  // },
 
-  basePath: "/keith-portfolio",
-  assetPrefix: "/keith-portfolio/",
+  // basePath: "/keith-portfolio",
+  // assetPrefix: "/keith-portfolio/",
 };
 
 export default nextConfig;
